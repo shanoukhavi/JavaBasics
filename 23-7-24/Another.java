7f276f391b6b41b102fabfa449d70d7930f3b0f7
shanoukhavi <shanoukh5@gmail.com>
2024-07-23 19:15:40 +0530

    done

---
+++ 23-7-24/Another.java
@@ -0,0 +1,18 @@
+
+
+public class Another {
+    public static void main(String[] args) {
+        //mu;tiple operator mate 
+        System.out.println(8-3*3);
+        //yes minus one is the answer mate 
+        //bodmas bracket order divide multiply addn subtraction 
+        System.out.println(9/3/3);
+        System.out.println(9/(3/3));
+        System.out.println(9/(3/3+2));
+        //the ans ois 3 mate  basics is gone mate otherwise here cant apply iy t o it 
+        //here the 9 is the ans mate otheriswe it is not psosibe to do it otherwise can do it 
+        //left to ryt mate precednece multiple / multiply addn subtraction mate otherwise wont work it her e
+        //it is from lft tto ryt mate otherwise another ois not psosisble amte 
+
+    }
+}
---
+++ 23-7-24/BasicOp.java
@@ -0,0 +1,27 @@
+public class BasicOp {
+public static void main(String[] args) {
+    //the modulou operator is used to get the remainder in t mate so been careful to fo it 
+    int a=9;
+    int b=6;
+    System.out.println(a+b);
+    //everything ac=can be seen mate 
+    System.out.println(a-b);
+    System.out.println(a*b);
+    System.out.println(a/b);
+    System.out.println(a%b);//here uw ill get he ans as 3 mate othewise not ther 
+    //yes mate 2 u will get 
+    //during the inti divisoon mate it is fone otherwise been carefult ot do it 
+    double x=5;
+    double y=3;
+    System.out.println(x+y);
+    System.out.println(x-y);
+    System.out.println(x*y);
+    System.out.println(x/y);
+    System.out.println(x%y);
+    //yes it is posisble mate it is the remainder thsi si tge integer diiion mate otherwise it snot possible 
+    //yes done mate outherie u cant fo it  everyhting is the double so been careful to o it mate  for the double its posisible amte so it can be done othwersiws ecant be applied mate been careful to do it 
+    //for the modulous it is not posiible mate othersiwse it was possible 
+
+    
+}    
+}
